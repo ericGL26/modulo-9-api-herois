09523a80ae5489cf21fc5e0f9cf8d77e7512c50b
Eduardo3007 <eduardo.oliveira@elastic.fit>
2023-11-14 16:32:16 -0300

    corrigir erro apijs

--- src/api.js
+++ src/api.js
@@ -1,29 +1,72 @@
-const Hapi = require('hapi')
-const Context = require('../db/strategies/base/contextStrategy')
-const MongoDb = require('../db/strategies/mongodb/mongodb')
-const HeroiSchema = require('../db/strategies/mongodb/schemas/heroisSchema')
-const HeroRoute = require('./routes/heroRoutes')
+const Hapi = require('hapi');
+const Hoek = require('@hapi/hoek');
+const Joi = require('joi');
+const Context = require('../db/strategies/base/contextStrategy');
+const MongoDb = require('../db/strategies/mongodb/mongodb');
+const HeroiSchema = require('../db/strategies/mongodb/schemas/heroisSchema');
+const HeroRoute = require('./routes/heroRoutes');
 
-const app = new Hapi.Server({
-    port: 8030
-})
-function mapRoutes(instance, methods) {
-    
-    return methods.map(method => instance[method]())
-}
+const HapiPino = require('hapi-pino');
 
 async function main() {
-    const connection = MongoDb.MongoDB.connect()
-    const context = new Context(new MongoDb.MongoDB(connection, HeroiSchema))
+    const connection = await MongoDb.MongoDB.connect();
+    const context = new Context(new MongoDb.MongoDB(connection, HeroiSchema));
 
-    app.route([
+    Hoek.assert(Joi, 'Joi not found');
+    const server = new Hapi.Server({
+        port: 8030
+    });
+
+    const logger = HapiPino({
+        prettyPrint: {
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+            errorLikeObjectKeys: ['err', 'error'],
+            messageKey: 'msg',
+            customPrettifiers: {
+                response: {
+                    key: 'res',
+                    response: true
+                },
+                request: {
+                    key: 'req',
+                    properties: ['method', 'url', 'headers', 'payload']
+                }
+            }
+        },
+        logEvents: ['response', 'onPostStart']
+    });
+
+    await server.register({
+        plugin: logger
+    });
+
+    server.route([
         ...mapRoutes(new HeroRoute(context), HeroRoute.methods())
-    ])
-    await app.start()
-    console.log('Servidor rodando na porta', app.info.port)
+    ]);
 
-    return app
+    await server.start();
+    console.log('Servidor rodando na porta', server.info.port);
 
+    return server;
 }
 
-module.exports = main()
+// Restante do código...
+
+
+
+async function start() {
+    try {
+        await main();
+    } catch (error) {
+        console.error('Erro ao iniciar o servidor:', error);
+        process.exit(1); // Encerrar o processo com código de erro
+    }
+}
+
+// Verificar se o arquivo está sendo executado diretamente (node api.js)
+if (require.main === module) {
+    start();
+}
+
+module.exports = start;
--- src/testes/apiHeroes.test.js
+++ src/testes/apiHeroes.test.js
@@ -1,42 +1,46 @@
 const assert = require('assert');
 const api = require('./../api');
-let app = {}
+let app = {};
+
+async function startServer() {
+    app = await api();
+}
+
 describe('Suite de testes da API heroes', function () {
-    this.beforeAll(async () => {
-        app = await api
-    })
+    before(async () => {
+        await startServer();
+    });
 
     it('Listar /herois', async () => {
         const result = await app.inject({
             method: 'GET',
             url: '/herois?skip=0&limit=10'
-        })
-        const dados = JSON.parse(result.payload)
-        const statusCode = result.statusCode
-        console.log('RESULTADO', result)
-        assert.equal(statusCode, 200)
+        });
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+        console.log('RESULTADO', result);
+        assert.equal(statusCode, 200);
+    });
 
-    })
     it('listar /herois - deve retornar um erro com limit incorreto', async () => {
-        const TAMANHO_LIMITE = 'AEEE'
+        const TAMANHO_LIMITE = 'AEEE';
         const result = await app.inject({
             method: 'GET',
             url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
-        })
-        assert.equal((result.payload), 'Error interno no servidor')
+        });
+        assert.equal(result.payload, 'Error interno no servidor');
+    });
 
-    })
     it('listar /herois - deve filtrar um item', async () => {
-        const NAME = 'Demons'
+        const NAME = 'Demons';
         const result = await app.inject({
             method: 'GET',
             url: `/herois?skip=0&limit=1000&nome=${NAME}`
-        })
-        
-        const dados = JSON.parse(result.payload)
-        const statusCode = result.statusCode
-        assert.equal(statusCode, 200)
-        assert.strictEqual(dados[0].nome, NAME);
+        });
 
-    })
-})
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+        assert.equal(statusCode, 200);
+        assert.strictEqual(dados[0].nome, NAME);
+    });
+});
